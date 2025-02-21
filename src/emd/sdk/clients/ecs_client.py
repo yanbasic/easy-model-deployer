9c1fdb31ddcffb31954eed9cc8c4a465f0fcfc1e
Tang Jie <outstandingcandy@gmail.com>
2025-02-21 18:04:43 +0800

    fix: add model id and model tag to all fastapi endpoint. (#28)
    * fix: add model id and model tag to all fastapi endpoint.
    
    * fix: ecs client path error.

--- src/emd/sdk/clients/ecs_client.py
+++ src/emd/sdk/clients/ecs_client.py
@@ -18,6 +18,7 @@ from emd.utils.aws_service_utils import check_stack_exists,get_model_stack_info
 from emd.models import Model
 from emd.constants import MODEL_DEFAULT_TAG
 from emd.utils.logger_utils import get_logger
+from emd.utils.framework_utils import get_model_specific_path
 import requests
 
 
@@ -114,7 +115,8 @@ class ECSClient(ClientBase):
 
     def invoke(self,pyload:dict):
         stream = pyload.get('stream', False)
-        url = f"{self.base_url}/invocations"
+        model_specific_invocations_path = get_model_specific_path(self.model_id, self.model_tag, "invocations")
+        url = f"{self.base_url}{model_specific_invocations_path}"
         if stream:
             response = requests.post(
                 url,
