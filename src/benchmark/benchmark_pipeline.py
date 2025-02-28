4d27df1e86ebc0dffe71ebc2b8aceabe956122ad
Ubuntu <ubuntu@ip-172-31-13-201.us-west-2.compute.internal>
2025-02-28 07:45:02 +0000

    feat: update pipeline.

--- src/benchmark/benchmark_pipeline.py
+++ src/benchmark/benchmark_pipeline.py
@@ -14,12 +14,12 @@ from common import start_benchmark_session
 
 session_time = 300
 ping_correction = True
-# max_users_list = [1, 5, 10]
-max_users_list = [1]
+max_users_list = [1, 5, 10]
+# max_users_list = [1]
 
 for max_users in max_users_list:
     args = argparse.Namespace(max_users=max_users, session_time=session_time, ping_correction=ping_correction)
     asyncio.run(start_benchmark_session(args, OpenAIStreamUserDef))
     print(f"Stream Benchmarking with {max_users} users completed.")
-    # asyncio.run(start_benchmark_session(args, OpenAIUserDef))
-    # print(f"Benchmarking with {max_users} users completed.")
+    asyncio.run(start_benchmark_session(args, OpenAIUserDef))
+    print(f"Benchmarking with {max_users} users completed.")
