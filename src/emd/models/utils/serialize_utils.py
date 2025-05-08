7047cae1fc47468bd748a684abc96607c9cf4375
zhouxss <zhouxss@amazon.com>
2025-05-08 12:47:50 +0000

    fix serialize_utils bugs

--- src/emd/models/utils/serialize_utils.py
+++ src/emd/models/utils/serialize_utils.py
@@ -2,14 +2,15 @@ import json
 import argparse
 
 JSON_DOUBLE_QUOTE_REPLACE = '<!>'
+JSON_SINGLE_QUOTE_REPLACE = '<*>'
 
 
 def load_extra_params(string):
-    string = string.replace(JSON_DOUBLE_QUOTE_REPLACE,'"')
+    string = string.replace(JSON_DOUBLE_QUOTE_REPLACE,'"').replace(JSON_SINGLE_QUOTE_REPLACE,"'")
     try:
         return json.loads(string)
     except json.JSONDecodeError:
         raise argparse.ArgumentTypeError(f"Invalid dictionary format: {string}")
 
 def dump_extra_params(d:dict):
-    return json.dumps(d).replace('"', JSON_DOUBLE_QUOTE_REPLACE)
+    return json.dumps(d).replace("'", JSON_SINGLE_QUOTE_REPLACE).replace('"', JSON_DOUBLE_QUOTE_REPLACE)
