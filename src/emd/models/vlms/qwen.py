fb9aab63c6ddb43b49c7cd611a7d8554154f44f3
zhouxss <zhouxss@amazon.com>
2025-05-14 05:55:59 +0000

    modify qwen3 deployment

--- src/emd/models/vlms/qwen.py
+++ src/emd/models/vlms/qwen.py
@@ -114,6 +114,44 @@ Model.register(
     )
 )
 
+
+
+Model.register(
+    dict(
+        model_id = "Qwen2.5-VL-7B-Instruct",
+        supported_engines=[vllm_qwen25vl72b_engine073],
+        supported_instances=[
+            g5d2xlarge_instance,
+            g5d4xlarge_instance,
+            g5d8xlarge_instance,
+            g5d12xlarge_instance,
+            g5d16xlarge_instance,
+            g5d24xlarge_instance,
+            g5d48xlarge_instance,
+            g6e2xlarge_instance,
+            local_instance
+        ],
+        supported_services=[
+            sagemaker_service,
+            sagemaker_async_service,
+            ecs_service,
+            local_service
+        ],
+        supported_frameworks=[
+            fastapi_framework
+        ],
+        allow_china_region=True,
+        huggingface_model_id="Qwen/Qwen2.5-VL-7B-Instruct",
+        modelscope_model_id="Qwen/Qwen2.5-VL-7B-Instruct",
+        require_huggingface_token=False,
+        application_scenario="vision llms for image understanding",
+        description="The latest series of Qwen2.5 VL",
+        model_type=ModelType.VLM,
+        model_series=QWEN2VL_SERIES
+    )
+)
+
+
 Model.register(
     dict(
         model_id = "QVQ-72B-Preview-AWQ",
@@ -156,7 +194,10 @@ Model.register(
             local_instance
         ],
         supported_services=[
-            sagemaker_service, sagemaker_async_service,local_service
+            sagemaker_service,
+            sagemaker_async_service,
+            ecs_service,
+            local_service
         ],
         supported_frameworks=[
             fastapi_framework
